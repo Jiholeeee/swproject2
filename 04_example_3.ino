ccbd7cbfd5e917cbfb83514caa1964fbe3e9aec6
Jiholeeee <145363965+Jiholeeee@users.noreply.github.com>
2023-09-21 02:07:14 +0900

    04_example_3.ino

---
+++ 04_example_3.ino
@@ -0,0 +1,27 @@
+#define PIN_LED 13
+
+int count = 0;
+int toggle = 0;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) {
+    ; 
+  }
+  Serial.println("Hello World!");
+  digitalWrite(PIN_LED, toggle);
+}
+
+void loop() {
+  Serial.println(++count);
+  toggle = toggle_state(toggle);
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+}
+
+int toggle_state(int toggle) {
+  return !toggle;
+}
+  // put your main code here, to run repeatedly:
+
